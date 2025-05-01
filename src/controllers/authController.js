4879512f1fccc20764fd31e65de25418f4b14933
Chaotic <124513444+Volburaal@users.noreply.github.com>
2025-05-01 19:12:37 +0500

    auth fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,15 +4,20 @@ export const login = async (req, res) => {
     try {
       const {pin} = req.body
       const isPasswordValid = await bcrypt.compare(pin, process.env.PIN_HASH);
-      const token = jwt.sign(
-        {name: "Seraphim"},
-        process.env.JWT_SECRET,
-        { expiresIn: "1h" }
-      );
-      res.json({
-        message: "Login successful",
-        token,
-      });
+      if (isPasswordValid){
+        const token = jwt.sign(
+          {name: "Seraphim"},
+          process.env.JWT_SECRET,
+          { expiresIn: "1h" }
+        );
+        res.json({
+          message: "Login successful",
+          token,
+        });
+      }
+      else{
+        res.status(400).json({message:"Incorrect Pin"})
+      }
     } catch (error) {
       console.error("Error during login:", error);
       res.status(500).json({ error: "Server error" });
